197a0f654658d7a39e943448d442de5ab2749ddf
KUSHPREET SINGH BAKSHI <97212963+Nerdysingh101101@users.noreply.github.com>
2024-01-25 19:36:38 +0000

    conversion

---
+++ firstscript/conversion.js
@@ -0,0 +1,35 @@
+let a = 15
+let b = "15"
+let c = "hello"
+let d = undefined
+let e = null
+let f = false
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof f);
+// console.log("after conversion");
+a1 = String(a)
+b1 = Number(b)
+c1 = Number(c)
+d1 = Number(d)
+e1 = Number(e)
+f1 = Number(f)
+// console.log(a1);
+// console.log(b1);
+// console.log(typeof c1);
+// console.log(typeof d1);
+// console.log(e1);
+// console.log(f1);
+
+//*****************************************operations*****************************************
+// 2 cases to see sum
+x1 = 10
+x2 = 5
+x3 = "5c"
+//case first in which string is last
+console.log(x1 + x2 + x3);
+//case second in which the string is first
+console.log(x3 + x2 + x1);
--- firstscript/helloworld.js
+++ firstscript/helloworld.js
@@ -1,1 +1,5 @@
 console.log("hello world")
+let x = 10
+console.log(`x:${x} before`);
+x++
+console.log(`x:${x} after`);
