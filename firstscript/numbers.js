3e670cc596cb2e7adc796675534bbe207b4d2ec5
KUSHPREET SINGH BAKSHI <97212963+Nerdysingh101101@users.noreply.github.com>
2024-02-05 17:35:26 +0000

    numbers and maths in js

---
+++ firstscript/numbers.js
@@ -0,0 +1,21 @@
+//two ways to define number, first by let keyword
+let abx = 1234.56
+let cbz = new Number(1234.5678)
+// console.log(typeof abx);
+// console.log(typeof cbz);
+//some methods
+// console.log(cbz.toLocaleString('en-IN'));
+// console.log(cbz.toPrecision(3));
+// console.log(cbz.toPrecision(4));
+// console.log(cbz.toPrecision(5).indexOf('3'));
+
+// ***************************************maths_library*******************************************************
+
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
