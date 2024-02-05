880c9caa0031921ee38b6339e2fe7a990e8bf8b2
KUSHPREET SINGH BAKSHI <97212963+Nerdysingh101101@users.noreply.github.com>
2024-02-05 17:52:33 +0000

    dates in js

---
+++ firstscript/dateandtime.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+console.log("to string "+myDate.toString());
+// console.log("to iso string "+myDate.toISOString());
+// console.log("to json "+myDate.toJSON());
+// console.log("to locale date str "+myDate.toLocaleDateString());
+// console.log("to locale time str "+myDate.toLocaleTimeString());
+console.log("to locale str "+myDate.toLocaleString());
+// console.log("to time str "+myDate.toTimeString());
+// console.log("to time str "+myDate.toDateString());
+
+//to be continued
